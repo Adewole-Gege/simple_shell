f1a4535cdab11b7fc1fdb40e5f98f65401d42a1f
Adewole-Gege <michaelgegs@gmail.com>
2024-08-28 05:06:40 -0700

    Changes made to fit betty-style as I have added descriptions

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include "main.h"
+/**
+* main - Entry point
+*
+* Description: At the momemt this is a program that is simple and passes the betty-style check.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
+/* My code comes here Adewole-Gege2k24 */
 return (0);
 }
