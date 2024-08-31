b32a59c9d1a45c177d80cc8eedc230b79cb39978
Adewole-Gege <michaelgegs@gmail.com>
2024-08-30 19:12:16 -0700

    task 0 betty ish

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
- * main - Entry point of the shell
- *
- * Return: 0 on success
- */
+* main - Entry point of the shell
+*
+* Return: 0 on success
+*/
 int main(void)
 {
-    /* TODO: Implement shell functionality */
-    return (0);
+/* TODO: Implement shell functionality */
+return (0);
 }
